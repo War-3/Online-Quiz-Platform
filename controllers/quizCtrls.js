b94eb27322ebed41f554440f9c523e921ef954a2
Walter <onyekweluwalter@gmail.com>
2024-10-06 22:38:54 +0100

    Updated

--- controllers/quizCtrls.js
+++ controllers/quizCtrls.js
@@ -1,127 +1,64 @@
-const quizArray  = require ('../models/quizModel')
+// a  Quiz Model that accept 10 questions with 4 options answers and one correct answer
+const Quiz  = require ('../models/quizModel')
 
-
-
-exports.quizFxn = async (req,res, next)=>{
-    
+exports.quizFxn = async (req, res, next) => {
     try {
-        const { userId, phoneNumber, category, quizId, quiz, quizAns} =req.body
+      const { userId, phoneNumber,category, samplesQuiz } = req.body;
 
-            const wans ="Incorrect!"
-            const rAns = "Correct!"
-           
-          
-           
-            if(quizAns[0].quizAns_1 =="A"){
-                quizAns[0].quizAns_1 = rAns
-            }else{
-                quizAns[0].quizAns_1=wans
-            }   
-            
-            if(quizAns[0].quizAns_2 =="D"){
-              quizAns[0].quizAns_2= rAns
-             }else{
-              quizAns[0].quizAns_2=wans
-             } 
-          
-             if(quizAns[0].quizAns_3 =="C"){
-              quizAns[0].quizAns_3= rAns
-             }else{
-              quizAns[0].quizAns_3=wans
-             } 
-          
-             if(quizAns[0].quizAns_4 =="A"){
-              quizAns[0].quizAns_4= rAns
-             }else{
-              quizAns[0].quizAns_4=wans
-             } 
-          
-             if(quizAns[0].quizAns_5 =="B"){
-              quizAns[0].quizAns_5= rAns
-             }else{
-              quizAns[0].quizAns_5=wans
-             } 
-          
-             if(quizAns[0].quizAns_6 =="C"){
-              quizAns[0].quizAns_6= rAns
-             }else{
-              quizAns[0].quizAns_6=wans
-             }  
-          
-             if(quizAns[0].quizAns_7 =="D"){
-              quizAns[0].quizAns_7= rAns
-             }else{
-              quizAns[0].quizAns_7=wans
-             } 
-            
-             if(quizAns[0].quizAns_8 =="C"){
-              quizAns[0].quizAns_8= rAns
-             }else{
-              quizAns[0].quizAns_8=wans
-             } 
-          
-             if(quizAns[0].quizAns_9 =="C"){
-              quizAns[0].quizAns_9= rAns
-             }else{
-              quizAns[0].quizAns_9=wans
-             } 
-             if(quizAns[0].quizAns_10 =="A"){
-              quizAns[0].quizAns_10= rAns
-             }else{
-              quizAns[0].quizAns_10=wans
-             } 
-             
-            
-             const exisitingUserId = await quizArray.findOne({ "userId": {
-                $elemMatch: {  firstName: `${userId[0].firstName}` }
-              } });
+      const incorrectMsg ="Incorrect!"
+      const correctMsg  = "Correct!"
+      const correctAnswers = ['A','B','D','A','D','C','C','B','B','D']  
 
-             if (exisitingUserId) {
-               return res.status(404).json({ message: "You have already Submitted!" });
-             }
-             
-        const exisitingUser = await quizArray.findOne({ phoneNumber });
 
-        if (exisitingUser) {
-          return res.status(404).json({ message: "You have already Submitted!" });
-        }
+      // Map through samplesQuiz and update the userAnswer based on correctness
+      samplesQuiz.forEach((quiz, index) => {
+      quiz.userAnswer = quiz.userAnswer === correctAnswers[index] ? correctMsg : incorrectMsg;
+      });
+            
         
+      // Check if the user has already submitted based on `userId` or `phoneNumber`
+      const exisitingUserId = await Quiz.findOne({ "userId": {
+        $elemMatch: {  firstName: `${userId[0].firstName}` }
+      } });
 
-        const newUser = new quizArray ({ userId, phoneNumber, category, quizId, quiz, quizAns} )
-    
-        await newUser.save()
-          
-        const correctAns = quizAns.reduce((count, item) => {
-            const correctCount = [
-              item.quizAns_1,
-              item.quizAns_2,
-              item.quizAns_3,
-              item.quizAns_4,
-              item.quizAns_5,
-              item.quizAns_6,
-              item.quizAns_7,
-              item.quizAns_8,
-              item.quizAns_9,
-              item.quizAns_10
-            ].filter(answer => answer === "Correct!").length
-            return count + correctCount;
-        }, 0);
-            
-    //    const correctAns = quizAns.filter(item=>item.quizAns_1,==="Correct!").length
-       let feedback = " "
-       if(correctAns >=5){
-        feedback ="Passed"
-      }else{
-        feedback = "Failed! Sorry you won't proceed to next stage of Program"
-       }
-    //    
-        return res.status(200).json({ message: "You have completed your exam", newUser,totalGrade: correctAns + "/10", feedback })
-        
-    } catch (error) {
-        return res.status(500).json({
-            message: error.message
-        })
-    }
-    next()
-  };
+     if (exisitingUserId) {
+       return res.status(404).json({ message: "You have already Submitted!" });
+     }
+     
+const exisitingUser = await Quiz.findOne({ phoneNumber });
 
+if (exisitingUser) {
+  return res.status(404).json({ message: "You have already Submitted!" });
+}
+
+
+const newUser = new Quiz ({ userId, phoneNumber,category, samplesQuiz} )
+
+await newUser.save()
+
+
+    // Calculate the total number of correct answers
+const correctAns = samplesQuiz.filter(item => item.userAnswer === correctMsg).length;
+
+
+let feedback = " "
+if(correctAns >=5){
+feedback ="Passed"
+}else{
+feedback = "Failed! Sorry you won't proceed to next stage of Program"
+}
+
+// Return the result
+return res.status(200).json({
+  message: "You have completed your exam",
+  newUser,
+  totalGrade: `${correctAns}/10`,
+  feedback
+});
+} catch (error) {
+return res.status(500).json({
+    message: error.message
+})
+}
+next()
+};
--- models/quizModel.js
+++ models/quizModel.js
@@ -7,48 +7,28 @@ const userIdSchema = new mongoose.Schema(
         state: {type: String,  require: true}
 })
 
-
-const quizSchema = new mongoose.Schema(
-    {
-        quiz_1: {type: String},
-        quiz_2: {type: String},
-        quiz_3: {type: String},
-        quiz_4: {type: String},
-        quiz_5: {type: String},
-        quiz_6: {type: String},
-        quiz_7: {type: String},
-        quiz_8: {type: String},
-        quiz_9: {type: String},
-        quiz_10: {type: String},
-    })
-    const quizAnsSchema = new mongoose.Schema(
-        {
-            quizAns_1: {type: String},
-            quizAns_2: {type: String},
-            quizAns_3: {type: String},
-            quizAns_4: {type: String},
-            quizAns_5: {type: String},
-            quizAns_6: {type: String},
-            quizAns_7: {type: String},
-            quizAns_8: {type: String},
-            quizAns_9: {type: String},
-            quizAns_10: {type: String},
-            
-        })
-   
-    const quizArraySchema = new mongoose.Schema(
-        {
+    const quizSchema = new mongoose.Schema({
             userId: [userIdSchema],
             phoneNumber: {type: Number, require: true},
-            category: {type: String},
-            quizId:{type: String},
-            quiz:[quizSchema],
-            quizAns:[quizAnsSchema]
-        });
+            category: String, 
+            quizId: String,
+            samplesQuiz: [
+                {
+                    question: String,
+                    options: {               
+                        A: String,
+                        B: String,
+                        C: String,
+                        D: String
+                        },
+            userAnswer: {  type: String, require: true }
+                    }]
+                
+});
 
 
-const  quizArray = new mongoose.model('quizArray',quizArraySchema)
+const  Quiz = new mongoose.model('Quiz',quizSchema)
 
 
-module.exports = quizArray
+module.exports = Quiz
 
